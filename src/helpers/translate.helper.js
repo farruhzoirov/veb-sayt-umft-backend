853699bf01862827e4f12e1a0777c433847243c5
farruhzoirov <fzoirov29@gmail.com>
2024-12-01 14:12:23 +0500

    commit

--- src/helpers/translate.helper.js
+++ src/helpers/translate.helper.js
@@ -3,28 +3,33 @@ const {getModelsTranslateHelper} = require("./get-models.helper");
 
 const BaseError = require('../errors/base.error');
 
+const Language  = require('../models/settings/language.model');
+
 
 const addTranslations = async (modelName, modelId, translationData) => {
     const forTranslateModel = TranslateModel[modelName]?.ref;
-    if (!forTranslateModel && !translationData.language) {
-        throw BaseError.NotFound("Translation model or language not found");
-        const dynamicTranslateModel = getModelsTranslateHelper(forTranslateModel);
-
-        const existingTranslation = await dynamicTranslateModel.findOne({
+    const dynamicTranslateModel = getModelsTranslateHelper(forTranslateModel);
+    const isModelTranslateEmpty = await dynamicTranslateModel.find();
+    if ( !isModelTranslateEmpty.length && !translationData.language) {
+        const defaultLanguage = await Language.findOne({isDefault: true});
+        const newTranslation = new dynamicTranslateModel({
             [modelName]: modelId,
-            language: translationData.language,
+            language: defaultLanguage._id,
+            ...translationData,
         });
-
-        if (!existingTranslation) {
-            const newTranslation = new dynamicTranslateModel({
-                [modelName]: modelId,
-                ...translationData,
-            });
-            return await newTranslation.save();
-        }
-
-        throw BaseError.BadRequest("Translation already exists for this language");
     }
+    const existingTranslation = await dynamicTranslateModel.findOne({
+        [modelName]: modelId,
+        language: translationData.language,
+    });
+    if (!existingTranslation) {
+        const newTranslation = new dynamicTranslateModel({
+            [modelName]: modelId,
+            ...translationData,
+        });
+        return await newTranslation.save();
+    }
+    throw BaseError.BadRequest("Translation already exists for this language");
 }
 
 const updateTranslations = async (modelName, modelId, translationData) => {
--- src/services/crud/add-models.service.js
+++ src/services/crud/add-models.service.js
@@ -3,11 +3,11 @@ const mongoose = require("mongoose");
 const BaseError = require('../../errors/base.error');
 
 // Static Models' names
-const { Model } = require("../../common/constants/models.constants");
+const {Model} = require("../../common/constants/models.constants");
 
-const { getModelsHelper } = require("../../helpers/get-models.helper");
+const {getModelsHelper} = require("../../helpers/get-models.helper");
 
-const { addTranslations } = require("../../helpers/translate.helper");
+const {addTranslations} = require("../../helpers/translate.helper");
 const populateModelData = require("../../helpers/populate.helper");
 
 class AddModelsService {
@@ -17,13 +17,12 @@ class AddModelsService {
 
     async addModel(modelName, modelData) {
         const dynamicModel = getModelsHelper(modelName);
-        const isSlugExists = await dynamicModel.findOne({ slug: modelData.slug });
+        const isSlugExists = await dynamicModel.findOne({slug: modelData.slug});
         if (isSlugExists) {
             throw BaseError.BadRequest('Slug already exists');
         }
         let newData;
         if (!modelData.modelId) {
-            console.log(modelData);
             if (modelName.trim() === 'language' && !modelData.isDefault) {
                 const languageExists = await dynamicModel.find();
                 if (!languageExists.length) {
@@ -40,22 +39,6 @@ class AddModelsService {
                 }).save();
                 return newData;
             }
-            if (modelName.trim() === 'language' && modelData.isDefault) {
-                const languageExists = await dynamicModel.find();
-                if (!languageExists.length) {
-                    newData = await new dynamicModel({
-                        ...modelData,
-                        img: modelData.img ? [modelData.img] : [],
-                    }).save();
-                    return newData;
-                }
-                newData = await new dynamicModel({
-                    ...modelData,
-                    img: modelData.img ? [modelData.img] : [],
-                }).save();
-                return newData;
-            }
-
             newData = await new dynamicModel({
                 ...modelData,
                 img: modelData.img ? [modelData.img] : [],
