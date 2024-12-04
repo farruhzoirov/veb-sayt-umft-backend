0acd32ea55e41c2fc271bdbe92c1f6184dbabc89
¨farruhzoirov¨¨ <fzoirov29@gmail.com>
2024-12-04 13:44:32 +0500

    commit

--- src/services/crud/add-models.service.js
+++ src/services/crud/add-models.service.js
@@ -1,76 +1,77 @@
 const mongoose = require("mongoose");
 const BaseError = require('../../errors/base.error');
 const {Model} = require("../../common/constants/models.constants");
-const { getModelsHelper } = require("../../helpers/get-models.helper");
+const {getModelsHelper} = require("../../helpers/get-models.helper");
 const {addTranslations} = require("../../helpers/translate.helper");
 const populateModelData = require("../../helpers/populate.helper");
 
 class AddModelsService {
-    constructor() {
-        this.Model = Model
+  constructor() {
+    this.Model = Model
+  }
+
+  async addModel(modelName, modelData) {
+    const dynamicModel = getModelsHelper(modelName);
+
+    const isSlugExists = await dynamicModel.findOne({slug: modelData.slug}).lean();
+
+    if (isSlugExists) {
+      throw BaseError.BadRequest('Slug already exists');
     }
 
-    async addModel(modelName, modelData) {
-        const dynamicModel = getModelsHelper(modelName);
+    let newData;
 
-        const isSlugExists = await dynamicModel.findOne({slug: modelData.slug}).lean();
-
-        if (isSlugExists) {
-            throw BaseError.BadRequest('Slug already exists');
+    if (!modelData.modelId) {
+      if (modelName.trim() === 'language' && !modelData.isDefault) {
+        const languageExists = await dynamicModel.find();
+        if (!languageExists.length) {
+          newData = await this.addingModelData(dynamicModel, modelData, true);
+          return newData;
         }
-
-        let newData;
-
-        if (!modelData.modelId) {
-            if (modelName.trim() === 'language' && !modelData.isDefault) {
-                const languageExists = await dynamicModel.find();
-                if (!languageExists.length) {
-                    newData = await this.addingModelData(dynamicModel, modelData, true);
-                    return newData;
-                }
-                newData = await this.addingModelData(dynamicModel, modelData);
-                return newData;
-            }
-
-            newData = await this.addingModelData(dynamicModel, modelData);
-
-            if (this.Model[modelName].translate) {
-                newData.translates = [await addTranslations(modelName, newData._id, modelData.translate)];
-            }
-            if (this.Model[modelName].populate) {
-                newData = await populateModelData(dynamicModel, newData._id, this.Model[modelName].populate);
-            }
-            return newData;
-        }
-
-        if (!mongoose.Types.ObjectId.isValid(modelData.modelId)) {
-            throw BaseError.BadRequest('Invalid modelId');
-        }
-
-        const existingModel = await dynamicModel.findById(modelData.modelId);
-        if (!existingModel) {
-            throw BaseError.NotFound("Model doesn't exist");
-        }
-        newData = existingModel;
-
-        if (this.Model[modelName].translate) {
-            newData.translates = [await addTranslations(modelName, modelData.modelId, modelData.translate)];
-        }
-        if (this.Model[modelName].populate) {
-            newData = await populateModelData(modelName, modelData.modelId, this.Model[modelName].populate);
-        }
+        newData = await this.addingModelData(dynamicModel, modelData);
         return newData;
+      }
+
+      newData = await this.addingModelData(dynamicModel, modelData);
+
+      if (this.Model[modelName].translate) {
+        newData.translates = [await addTranslations(modelName, newData._id, modelData.translate)];
+      }
+
+      if (this.Model[modelName].populate) {
+        newData = await populateModelData(dynamicModel, newData._id, this.Model[modelName].populate);
+      }
+      return newData;
     }
 
-    async addingModelData(dynamicModel, modelData, isDefault = false) {
-        const savedDocument = await new dynamicModel({
-            ...modelData,
-            isDefault: isDefault,
-            img: modelData.img ? [modelData.img] : [],
-        }).save();
-        savedDocument.toObject();
-        return savedDocument;
+    if (!mongoose.Types.ObjectId.isValid(modelData.modelId)) {
+      throw BaseError.BadRequest('Invalid modelId');
     }
+
+    const existingModel = await dynamicModel.findById(modelData.modelId);
+    if (!existingModel) {
+      throw BaseError.NotFound("Model doesn't exist");
+    }
+    newData = existingModel;
+
+    if (this.Model[modelName].translate) {
+      newData.translates = [await addTranslations(modelName, modelData.modelId, modelData.translate)];
+    }
+    if (this.Model[modelName].populate) {
+      newData = await populateModelData(modelName, modelData.modelId, this.Model[modelName].populate);
+    }
+    return newData;
+  }
+
+  async addingModelData(dynamicModel, modelData, isDefault = false) {
+    const savedDocument = await new dynamicModel({
+      ...modelData,
+      isDefault: isDefault,
+      img: modelData.img ? [modelData.img] : [],
+    }).save();
+    savedDocument.toObject();
+    return savedDocument;
+  }
 }
 
 module.exports = AddModelsService;
