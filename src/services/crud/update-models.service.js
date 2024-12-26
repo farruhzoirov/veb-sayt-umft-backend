bd948561802607e73adfed86e643b2b246f01619
farruhzoirov <fzoirov29@gmail.com>
2024-12-26 14:13:23 +0500

    commit

--- src/services/crud/update-models.service.js
+++ src/services/crud/update-models.service.js
@@ -1,15 +1,14 @@
 const mongoose = require("mongoose");
 
-const {Model} = require("../../common/constants/models.constants");
+const { Model } = require("../../common/constants/models.constants");
 
-const {updateTranslations} = require("../../helpers/translate.helper");
+const { updateTranslations } = require("../../helpers/translate.helper");
 
-const {getModelsHelper} = require("../../helpers/get-models.helper");
+const { getModelsHelper } = require("../../helpers/get-models.helper");
 
 const BaseError = require("../../errors/base.error");
 
 const populateModelData = require("../../helpers/populate.helper");
-
 
 class UpdateModelsService {
   constructor() {
@@ -18,7 +17,7 @@ class UpdateModelsService {
 
   async updateModel(modelName, modelId, updateData) {
     if (!mongoose.Types.ObjectId.isValid(modelId)) {
-      throw BaseError.BadRequest('Invalid modelId');
+      throw BaseError.BadRequest("Invalid modelId");
     }
     let newData;
     const dynamicModel = getModelsHelper(modelName);
@@ -28,8 +27,10 @@ class UpdateModelsService {
       throw BaseError.BadRequest("Model doesn't exist");
     }
 
-    if (modelName.trim() === 'language' && updateData.isDefault) {
-      const isDefaultLanguageExists = await dynamicModel.find({isDefault: true});
+    if (modelName.trim() === "language" && updateData.isDefault) {
+      const isDefaultLanguageExists = await dynamicModel.find({
+        isDefault: true,
+      });
       if (isDefaultLanguageExists.length) {
         for (const element of isDefaultLanguageExists) {
           element.isDefault = false;
@@ -37,31 +38,30 @@ class UpdateModelsService {
         }
       }
     }
-    newData = await dynamicModel.findOneAndUpdate({
-        _id: modelId
-      },
-      {
-        $set: updateData
-      },
-      {
-        new: true
-      }
+    newData = await dynamicModel.findOneAndUpdate(
+      { _id: modelId },
+      { $set: updateData },
+      { new: true }
     );
     newData = newData.toObject();
     delete newData.__v;
-    // Update translations
     if (this.Model[modelName].populate) {
-      newData = await populateModelData(dynamicModel, modelId, this.Model[modelName].populate);
+      newData = await populateModelData(
+        dynamicModel,
+        modelId,
+        this.Model[modelName].populate
+      );
     }
-
     if (updateData.translate) {
-      const translations = await updateTranslations(modelName, modelId, updateData.translate);
+      const translations = await updateTranslations(
+        modelName,
+        modelId,
+        updateData.translate
+      );
       newData.translates = translations;
-  
     }
     return newData;
   }
 }
-
 
 module.exports = UpdateModelsService;
