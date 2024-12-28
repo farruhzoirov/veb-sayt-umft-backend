e032bf0564433bdbd4a9cf611ceba255e04bf9bb
farruhzoirov <fzoirov29@gmail.com>
2024-12-28 18:02:40 +0500

    commit

---
+++ src/services/crud/check-slug.service.js
@@ -0,0 +1,15 @@
+const {getModelsHelper} = require("../../helpers/get-models.helper");
+
+
+class CheckIsSlugExistsService {
+    async checkIsSlugExists(model,slug) {
+        const dynamicModel = await getModelsHelper(model);
+        const isSlugExists = await dynamicModel.find({slug: slug}).lean();
+        if (!isSlugExists) {
+            return 0;
+        }
+        return 1;
+    }
+}
+
+module.exports = CheckIsSlugExistsService;
