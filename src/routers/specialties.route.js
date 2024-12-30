654ea56300473eacdb15b51d323d4541e3cbe834
farruhzoirov <fzoirov29@gmail.com>
2024-12-30 13:43:38 +0500

    commit

--- src/routers/specialties.route.js
+++ src/routers/specialties.route.js
@@ -1,7 +1,5 @@
 const router = require('express').Router();
-const upload = require('../utils/uploads/upload-models.util')
 const authMiddleware = require('../middlewares/auth.middleware');
-
 
 const SpecialtyController = require('../controllers/specialties/specialty.controller');
 
@@ -9,13 +7,5 @@ const specialtiesController = new SpecialtyController();
 
 router.get('/fetch-specialty', authMiddleware.adminMiddleware, specialtiesController.fetchSpecialties);
 
-router.get('/get-specialties', authMiddleware.adminMiddleware, specialtiesController.getSpecialties);
-
-router.get('/get-specialty/:id', authMiddleware.adminMiddleware, specialtiesController.getOneSpecialty);
-
-
-router.put('/update-specialty/:id', authMiddleware.adminMiddleware, upload.fields([
-  {name: 'image', maxCount: 1}
-]), specialtiesController.updateSpecialty);
 
 module.exports = router;
