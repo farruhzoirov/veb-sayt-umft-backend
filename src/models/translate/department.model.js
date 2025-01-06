6c60f44312cbf5c0663619b596bb6717bb96fb46
farruhzoirov <fzoirov29@gmail.com>
2025-01-06 15:51:51 +0500

    commit

--- src/models/translate/department.model.js
+++ src/models/translate/department.model.js
@@ -7,6 +7,9 @@ const DepartmentTranslateSchema = new Schema({
        type: String,
        required: true,
    },
+    description: {
+       type: String
+    },
     text: {
         type: String
     },
