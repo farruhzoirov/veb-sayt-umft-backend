ec8c5dd4331f7c75552ffef1df7bef6a03d530ee
farruhzoirov <fzoirov29@gmail.com>
2025-01-11 16:13:43 +0500

    commit

--- src/swagger.js
+++ src/swagger.js
@@ -1,10 +1,16 @@
 const swaggerUi = require('swagger-ui-express');
 const YAML = require('yamljs');
 const AdminPanelSwaggerDocument = YAML.load('./swagger/admin-panel-swagger.yaml');
-// const UiSwaggerDocument = YAML.load('../swagger/ui-swagger.yaml');
-
+const UiSwaggerDocument = YAML.load('./swagger/ui-swagger.yaml');
 
 module.exports = (app) => {
-    app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(AdminPanelSwaggerDocument));
-    // app.use("/api-frontend-docs", swaggerUi.serve, swaggerUi.setup(UiSwaggerDocument));
-}
+    // Serve Admin Panel Swagger UI
+    app.use('/api-docs', swaggerUi.serve, (req, res, next) => {
+        return swaggerUi.setup(AdminPanelSwaggerDocument)(req, res, next);
+    });
+
+    // Serve UI Swagger UI
+    app.use('/api-frontend-docs', swaggerUi.serve, (req, res, next) => {
+        return swaggerUi.setup(UiSwaggerDocument)(req, res, next);
+    });
+};
