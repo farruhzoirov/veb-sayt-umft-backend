f8cf12b41172d6b9453dbb0e24fef18acb747821
farruhzoirov <fzoirov29@gmail.com>
2025-01-29 12:27:03 +0500

    commit

--- src/services/frontend/news.service.js
+++ src/services/frontend/news.service.js
@@ -18,57 +18,42 @@ class NewsService {
     const dynamicModel = getModelsHelper(currentModel);
     let newsList;
 
-    // Query preparation
     const queryParameters = {
       limit: req.query?.limit ? parseInt(req.query.limit, 10) : 30,
       page: req.query?.page ? parseInt(req.query.page, 10) : 1,
-      skip: (req.query?.limit ? parseInt(req.query.limit, 10) : 10) * ((req.query.page ? parseInt(req.query.page, 10) : 1) - 1),
+      skip: (parseInt(req.query?.limit, 10) || 10) * ((parseInt(req.query.page, 10) || 1) - 1),
       selectFields: req.query?.select || '',
       requestedLanguage: req.query?.language || defaultLanguage.slug,
       category: req.query?.category,
     };
 
-    const selectedLanguage = await Language.findOne({
-      slug: queryParameters.requestedLanguage,
-    }).lean();
+    const selectedLanguage = await Language.findOne({slug: queryParameters.requestedLanguage}).lean();
 
     if (!selectedLanguage) {
       throw BaseError.BadRequest("Language doesn't exists which matches to this slug");
     }
 
-    if (queryParameters.category && !(typeof queryParameters.category !== 'string' || !Array.isArray(queryParameters.category))) {
+    if (queryParameters.category && typeof queryParameters.category !== 'string' && !Array.isArray(queryParameters.category)) {
       throw BaseError.BadRequest("Category must be string or an array");
     }
 
-    if (!queryParameters.category && !Array.isArray(queryParameters?.category)) {
-      console.log('1');
+    let categoryIds;
+    if (queryParameters.category) {
+      const categories = Array.isArray(queryParameters.category) ? queryParameters.category : [queryParameters.category];
+      categoryIds = await Category.find({slug: {$in: categories}}).distinct('_id').lean();
+    }
+    const filter = {status: 1};
 
-      newsList = await dynamicModel
-          .find({status: 1})
-          .sort({_id: -1})
-          // .select(queryParameters.selectFields ? queryParameters.selectFields : `-__v  -updatedAt -active -status`)
-          .limit(queryParameters.limit)
-          .skip(queryParameters.skip)
-          .lean();
+    if (categoryIds.length) {
+      filter.category = {$in: categoryIds};
     }
 
-    if (Array.isArray(queryParameters.category) || queryParameters.category) {
-      console.log('2');
-
-      const categories = Array.isArray(queryParameters.category)
-          ? queryParameters.category
-          : [queryParameters.category];
-
-      const categoriesId = await Category.find({slug: {$in: categories}}).distinct("_id").lean();
-
-      newsList = await dynamicModel
-          .find({status: 1, category: {$in: categoriesId}})
-          .sort({_id: -1})
-          // .select(queryParameters.selectFields ? queryParameters.selectFields : `-__v  -updatedAt -active -status`)
-          .limit(queryParameters.limit)
-          .skip(queryParameters.skip)
-          .lean();
-    }
+    newsList = await dynamicModel
+        .find(filter)
+        .sort({_id: -1})
+        .limit(queryParameters.limit)
+        .skip(queryParameters.skip)
+        .lean()
 
     if (this.Model[currentModel].translate) {
       const translateModelName = this.TranslateModel[currentModel].ref;
@@ -79,24 +64,23 @@ class NewsService {
             const translationData = await dynamicTranslateModel.findOne({
               [currentModel]: modelItem._id,
               [this.Model.language.ref]: selectedLanguage._id,
-            })
-            // .select(queryParameters.selectFields ? queryParameters.selectFields : `-${currentModel} -__v -language  -updatedAt`).lean();
-
-            return {...modelItem, ...translationData || {}};
+            }).select(`-${currentModel} -__v -language  -updatedAt`).lean();
+            return {...modelItem, ...(translationData || {})};
           })
       );
     }
     newsList = newsList.filter((item) => item.title);
     const total = await dynamicModel.countDocuments({status: 1});
 
-    const paginationInfo = {
-      total: total,
-      limit: queryParameters.limit,
-      page: queryParameters.page,
-      pages: Math.ceil(total / queryParameters.limit),
+    return {
+      data: newsList,
+      pagination: {
+        total,
+        limit: queryParameters.limit,
+        page: queryParameters.page,
+        pages: Math.ceil(total / queryParameters.limit),
+      },
     };
-
-    return {data: newsList, pagination: paginationInfo};
   }
 }
 
