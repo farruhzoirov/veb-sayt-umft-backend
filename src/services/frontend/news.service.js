5c54a6d79ab20ddcdcdee032761c5500867e5b35
farruhzoirov <fzoirov29@gmail.com>
2025-01-29 10:54:19 +0500

    commit

--- src/services/frontend/news.service.js
+++ src/services/frontend/news.service.js
@@ -36,9 +36,9 @@ class NewsService {
       throw BaseError.BadRequest("Language doesn't exists which matches to this slug");
     }
     console.log(queryParameters.category);
-    if (queryParameters.category &&
-        typeof (queryParameters.category) !== 'string' ||
-        !Array.isArray(queryParameters.category)
+    if (queryParameters.category
+        // typeof (queryParameters.category) !== 'string' ||
+        // !Array.isArray(queryParameters.category)
     ) {
       console.log('Category not found');
       throw BaseError.BadRequest("Category must be string or an array");
