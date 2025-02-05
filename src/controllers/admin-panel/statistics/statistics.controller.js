c5c29b7c116fd8b440b44c41797e8a2a17d394e2
farruhzoirov <fzoirov29@gmail.com>
2025-02-05 12:18:17 +0500

    commit

--- src/controllers/admin-panel/statistics/statistics.controller.js
+++ src/controllers/admin-panel/statistics/statistics.controller.js
@@ -1,16 +1,16 @@
-const News = require('../../../models/data/news.model');
-const Category = require('../../../models/data/category.model');
-const Language = require('../../../models/settings/language.model');
-const Events = require('../../../models/data/events.model');
-const EventsCategory = require('../../../models/data/events-category.model');
-
-const Employee = require('../../../models/data/employee.model');
-const Specialty = require('../../../models/specialty/specialty.model');
-const Department = require('../../../models/data/department.model');
-const Applicants = require('../../../models/applicants/applicants.model');
-
-const Degree = require('../../../models/data/degrees.model');
-const Partner = require('../../../models/data/partners.model');
+const models = {
+  news: require('../../../models/data/news.model'),
+  category: require('../../../models/data/category.model'),
+  language: require('../../../models/settings/language.model'),
+  events: require('../../../models/data/events.model'),
+  eventsCategory: require('../../../models/data/events-category.model'),
+  employee: require('../../../models/data/employee.model'),
+  specialty: require('../../../models/specialty/specialty.model'),
+  department: require('../../../models/data/department.model'),
+  applicants: require('../../../models/applicants/applicants.model'),
+  degree: require('../../../models/data/degrees.model'),
+  partner: require('../../../models/data/partners.model')
+};
 
 const Logger = require('../../../models/logger/logger.model');
 
@@ -19,36 +19,23 @@ class StatisticController {
     this.getUrlStatistics = this.getUrlStatistics.bind(this);
     this.ModelCounts = this.ModelCounts.bind(this);
   }
+
   async ModelCounts(req, res, next) {
     try {
-      const languages = await Language.countDocuments() || 0;
-      const categories = await Category.countDocuments() || 0;
-      const news = await News.countDocuments() || 0;
-      const events = await Events.countDocuments() || 0;
-      const eventsCategory = await EventsCategory.countDocuments() || 0;
-      const employees = await Employee.countDocuments() || 0;
-      const specialties = await Specialty.countDocuments() || 0;
-      const departments = await Department.countDocuments() || 0;
-      const applicants = await Applicants.countDocuments() || 0;
-      const degrees = await Degree.countDocuments() || 0;
-      const partners = await Partner.countDocuments() || 0;
+      let statistics = {};
 
-      const newsRegex = "/front/news/*";
-      const newsRequestCounts = await this.getUrlStatistics(newsRegex);
-      res.status(200).json({
-        languages: languages,
-        categories: categories,
-        news: news,
-        events: events,
-        eventsCategory: eventsCategory,
-        employees: employees,
-        specialties: specialties,
-        departments: departments,
-        applicants: applicants,
-        degrees: degrees,
-        partners: partners,
-        newsRequestCounts: newsRequestCounts
-      });
+      // **Model obyektidan dinamik hisoblash**
+      for (const [key, model] of Object.entries(models)) {
+        statistics[key] = await model.countDocuments() || 0;
+      }
+
+      // **URL statistikalarini olish**
+      for (const key of Object.keys(models)) {
+        const urlRegex = new RegExp(`/front/${key}/*`, "i"); // `/front/model_name/*` kabi url
+        statistics[`${key}RequestCounts`] = await this.getUrlStatistics(urlRegex);
+      }
+
+      res.status(200).json(statistics);
     } catch (error) {
       next(error);
     }
@@ -56,26 +43,10 @@ class StatisticController {
 
   async getUrlStatistics(regex) {
     try {
-      // const logs = await Logger.aggregate([
-      //   {
-      //     $match: {
-      //       url: {$regex: regex, $options: 'i'},
-      //     },
-      //   }, {
-      //   $group: {
-      //     _id: "$url",
-      //     count: { $sum: 1 },
-      //   }
-      //   },
-      //   {
-      //     $sort: {count: -1}
-      //   }
-      // ]);
-
-      const logs = await Logger.countDocuments({url: {$regex: regex, $options: 'i'}})
-      return logs;
+      return await Logger.countDocuments({ url: { $regex: regex, $options: 'i' } });
     } catch (error) {
-      console.log("Error getting url based statistics",error);
+      console.log("Error getting URL statistics:", error);
+      return 0;
     }
   }
 }
